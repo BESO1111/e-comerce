c4aefdad205d9c097e608f935dbbd7631fc57d9a
BESO1111 <bkatamadze4@gmail.com>
2022-11-14 17:46:34 +0400

    e-comerce.ar aris dasrulebuli proeqti

---
+++ script/js.js
@@ -0,0 +1,160 @@
+// var nivti=['televizori','kamera','klaviatura'];
+
+// var x=0;
+
+// for(i=0; i<nivti.length; i++){
+
+// x= x+nivti.length
+
+// }
+
+// var x=0
+
+// function damateba(){
+
+// document.getElementById("kalata").innerHTML=x+=1
+
+// }
+
+// var x="";
+// for(i=1;i<6; i++){
+// x+=i
+//    if(i===4)continue
+
+//     console.log(x)
+// }
+
+
+
+
+
+
+
+let heart=[];
+document.getElementById("shenaxuli").innerHTML=heart;
+
+let favorite=[];
+document.getElementById("kalataa").innerHTML=favorite;
+
+
+
+
+
+
+ let televizori={
+    name:'televizori',
+
+brand:'asus',
+fasi:500,
+ganvateba:100,
+ricxvi: Math.random,
+ }
+ let kamera={
+    name:'kamera',
+    brand:'nokia',
+    fasi:500,
+    ganvateba:100,
+    ricxvi: Math.random,
+     }
+
+     let klaviatura={
+        name:'klaviatura',
+
+        brand:'apple',
+        fasi:500,
+        ganvateba:100,
+        ricxvi: Math.random,}
+
+
+         function favoriti(){
+
+p1=heart.push("televizori")
+
+
+document.getElementById("shenaxuli").innerHTML=p1
+
+  }
+  
+  function favoriti(){
+
+    p2=heart.push("kamera")
+    document.getElementById("shenaxuli").innerHTML=p2
+
+   
+      }
+
+
+      
+      function favoriti(){
+
+        p3=heart.push("klaviatura")
+        
+        
+        document.getElementById("shenaxuli").innerHTML=p3
+          }
+        
+         function kalata(){
+            q1=favorite.push("televizori")
+        
+          
+            document.getElementById("kalataa").innerHTML=q1
+
+
+         }
+         
+         function kalata(){
+            q2=favorite.push("klaviatura")
+        
+          
+            document.getElementById("kalataa").innerHTML=q2
+
+
+         }
+         
+         function kalata(){
+            q3=favorite.push("kamera")
+        
+          
+            document.getElementById("kalataa").innerHTML=q3
+
+
+         }
+
+
+
+
+
+       // gaxsna daxurva klikebis dros 
+
+       function showshenaxuli(){
+
+        document.getElementById("shenaxvanivti").style.display="block";
+       }
+
+
+
+       function showkalata(){
+
+document.getElementById("shenaxvakalata").style.display="block";
+
+       }
+
+
+       function clozex(){
+
+document.getElementById("shenaxvanivti").style.display="none";
+
+       }
+
+
+       function clozey(){
+
+         document.getElementById("shenaxvakalata").style.display="none";
+         
+                }
+         
+
+
+               // აქ გავიჭედე უკვე ამის მეტი ვეღარ გავაკეთე რამე.
+
+ 
